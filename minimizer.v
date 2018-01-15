8468424054e7cc1df7783b4e181e3072316f6c80
Dominique Larchey-Wendling <larchey@wendling>
2018-01-15 17:34:06 +0100

    minimizer for computable predicates

--- minimizer.v
+++ minimizer.v
@@ -46,6 +46,82 @@ Section nat_rev_ind'.
 
 End nat_rev_ind'.
 
+Section minimizer_pred.
+
+  Variable (P : nat -> Prop)
+           (HP : forall p: { n | P n \/ ~ P n }, { P (proj1_sig p) } + { ~ P (proj1_sig p) }).
+
+  Definition minimizer n := P n /\ forall i, i < n -> ~ P i.
+
+  Inductive bar n : Prop :=
+    | in_bar_0 : P n -> bar n
+    | in_bar_1 : ~ P n -> bar (S n) -> bar n.
+
+  Let bar_ex n : bar n -> P n \/ ~ P n.
+  Proof. induction 1; auto. Qed.
+
+  Let loop : forall n, bar n -> { k | P k /\ forall i, n <= i < k -> ~ P i }.
+  Proof.
+    refine (fix loop n Hn { struct Hn } := match HP (exist _ n (bar_ex Hn)) with
+      | left H => exist _ n _
+      | right H => match loop (S n) _ with 
+        | exist _ k Hk => exist _ k _
+      end 
+    end).
+    * split; auto; intros; omega.
+    * destruct Hn; [ destruct H | ]; assumption.
+    * destruct Hk as (H1 & H2).
+      split; trivial; intros i Hi.
+      destruct (eq_nat_dec i n).
+      - subst; trivial.
+      - apply H2; omega.
+  Qed.
+
+  Hypothesis Hmin : ex minimizer.
+
+  Let bar_0 : bar 0.
+  Proof.
+    destruct Hmin as (k & H1 & H2).
+    apply in_bar_0 in H1.
+    clear HP.
+    revert H1.
+    apply nat_rev_ind' with (k := k).
+    intros i H3.
+    apply in_bar_1, H2; trivial.
+    omega.
+  Qed.
+
+  Definition minimizer_pred : sig minimizer.
+  Proof.
+    destruct (loop bar_0) as (k & H1 & H2).
+    exists k; split; auto.
+    intros; apply H2; omega.
+  Defined.
+
+End minimizer_pred.
+
+Check minimizer_pred.
+Print Assumptions minimizer_pred.
+Recursive Extraction minimizer_pred.
+
+(** Let P be a computable predicate:
+      - whenever P n has a value (P n or not P n) then that value can be computed
+    Then minimizer P is computable as well:
+      - whenever minimizer P holds for some n, then such an n can be computed
+ *)
+      
+
+Corollary minimizer_alt (P : nat -> Prop) :
+    (forall n, P n \/ ~ P n -> { P n } + { ~ P n }) -> ex (minimizer P) -> sig (minimizer P).
+Proof.
+  intro H; apply minimizer_pred.
+  intros (n & Hn); apply H, Hn.
+Defined.
+
+Check minimizer_alt.
+Print Assumptions minimizer_alt.
+Recursive Extraction minimizer_alt.
+
 Section minimizer.
 
   Variable (R : nat -> nat -> Prop)
